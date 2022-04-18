da272d99a2e52d251a484fb4bdf2cc44e35b09af
gus-sm <gustavo1sad@gmail.com>
2022-04-18 12:37:17 -0300

    Injecao de dependencia de helper csvStreamReader ao inves de importacao estatica

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,19 +1,19 @@
 const multer  = require('multer'),
 
 fileUpload = require('../middleware/uploadMiddleware'),
-csvStream = require('../helpers/csvStream'); 
 
 config = require('../config/config');
 
 
-const uploadController = () => {
+const uploadController = (csvStreamReader) => {
     let message = '';
-    function uploadForm(req, res){
+    function uploadFormView(req, res){
         res.status(200).render('index');
     }
 
 
     function uploadHandler(req, res){
+        
         const upload = multer({
             storage: fileUpload.files.storage(), 
             fileFilter: fileUpload.files.allowedFile 
@@ -24,19 +24,24 @@ const uploadController = () => {
                 message = err;
                 return res.status(400).render('index',{message: message, error: true});
             }
+            if(!req.file){
+                message = config.NO_FILE_ERROR;
+                return res.status(400).render('index',{message: message, error: true});
+            }
 
             message = config.SUCCESS_UPLOAD_MESSAGE;
 
-            req.file?
-                console.log(`Nome do arquivo: ${req.file.originalname}\nTamanho: ${req.file.size}B`) : ''
 
-            csvStream(req.file.path,function(data){console.log(data)});
+            csvStreamReader(req.file.path,function(data){
+                console.log(data);
+            });
+
             return res.status(200).render('index',{message: message});
 
         });
     }
 
-    return {uploadForm, uploadHandler};
+    return {uploadFormView, uploadHandler};
 }
 
 module.exports = uploadController;
