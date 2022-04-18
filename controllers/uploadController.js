5cb64df60c3aae2e9cb4da4d500c73605b5283f8
gus-sm <gustavo1sad@gmail.com>
2022-04-18 01:14:19 -0300

    Criacao de imagem docker para mongodb

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,12 +1,10 @@
 const multer  = require('multer'),
-<<<<<<< HEAD
+
 fileUpload = require('../middleware/uploadMiddleware'),
 csvStream = require('../helpers/csvStream'); 
-=======
-fileUpload= require('../middleware/uploadMiddleware'),
+
 config = require('../config/config');
 
->>>>>>> 6c10445492629241497a983e27bbc9b35c8e800b
 
 const uploadController = () => {
     let message = '';
@@ -24,23 +22,17 @@ const uploadController = () => {
         upload(req, res, (err) => {
             if (err) {
                 message = err;
-<<<<<<< HEAD
                 return res.status(400).render('index',{message: message, error: true});
             }
 
-            message = 'Arquivo aceito!';
+            message = config.SUCCESS_UPLOAD_MESSAGE;
+
+            req.file?
+                console.log(`Nome do arquivo: ${req.file.originalname}\nTamanho: ${req.file.size}B`) : ''
+
             csvStream(req.file.path,function(data){console.log(data)});
             return res.status(200).render('index',{message: message});
 
-=======
-             } else{
-                 message = config.SUCCESS_UPLOAD_MESSAGE;
-             }
-            req.file?
-                console.log(`Nome do arquivo: ${req.file.originalname}\nTamanho: ${req.file.size}B`) : ''
-            return res.render('index',{message: message});
-             
->>>>>>> 6c10445492629241497a983e27bbc9b35c8e800b
         });
     }
 
