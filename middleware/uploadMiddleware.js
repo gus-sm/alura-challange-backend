0615ef01adde5a64588837832f75ab57badabbac
gus-sm <gustavo1sad@gmail.com>
2022-04-18 01:15:50 -0300

    Corrigido conflitos

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 const multer = require('multer');
-=======
-const multer = require('multer'),
+
 config = require('../config/config');
->>>>>>> 6c10445492629241497a983e27bbc9b35c8e800b
+
 
 module.exports.files = {
     storage: function () {
@@ -20,11 +18,8 @@ module.exports.files = {
     },
     allowedFile: function (_, file, cb) {
         if (!file.originalname.match(/\.(csv)$/)) {
-<<<<<<< HEAD
-            return cb(new Error("Apenas arquivos CSV são aceitos!"));
-=======
             return cb(new Error(config.NOT_ACCEPTED_ERROR));
->>>>>>> 6c10445492629241497a983e27bbc9b35c8e800b
+
         }
         cb(null, true);
     }
