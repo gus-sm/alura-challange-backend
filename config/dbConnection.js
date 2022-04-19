a4fda5b967b58c26c509506876fad45ab50d33fc
gus-sm <gustavo1sad@gmail.com>
2022-04-19 13:41:31 -0300

    Criacao de arquivo para conexao no mongodb via mongoose

---
+++ config/dbConnection.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+
+const Connection = () => {
+    const db = mongoose.connection;
+
+    db.once('open', function () {
+      console.log("db connected!");
+    });
+    db.once('close', function(){
+        console.log("db closed!");
+    })
+    
+    async function openConnection(){
+
+        try {
+            await mongoose.connect('mongodb://alurachallange:135790@localhost:27017/Transactions');
+        
+        } catch (err){
+            throw new Error(err);
+        }
+    }
+
+
+    async function closeConnection(){
+        try{
+            await db.close();
+            
+        } catch(err){
+            throw new Error(err);
+        }
+    }
+
+    function getDbConn(){
+        return db;
+    }
+    
+    return {openConnection,closeConnection,getDbConn};
+}
+
+module.exports = { Connection };
