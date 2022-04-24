a73f8717ea3ecc485c8ffbf200583c04e79d98db
gus-sm <gustavo1sad@gmail.com>
2022-04-24 07:25:53 -0300

    Renomeado arquivo Upload

---
+++ controllers/Uploads.js
@@ -0,0 +1,33 @@
+const multer  = require('multer'),
+fileUpload = require('../middleware/uploadSettings'),
+config = require('../config/config');
+
+const Upload = () => {
+    let message = '';
+
+    function uploadHandler(req, res, next){
+
+        const upload = multer({
+            storage: fileUpload.files.storage(), 
+            fileFilter: fileUpload.files.allowedFile 
+        }).single('filepond');
+
+        upload(req, res, (err) => {
+            if (err) {
+                message = err;
+                return res.status(400).render('index',{message: message, error: true});
+            }
+            if(!req.file){
+                message = config.NO_FILE_ERROR;
+                return res.status(400).render('index',{message: message, error: true});
+            }
+            req.message = config.TRANSACTION_SUCCESS_MESSAGE;
+            next();
+            //return res.status(200).render('index',{message: config.TRANSACTION_SUCCESS_MESSAGE});
+        });
+    }
+
+    return {uploadHandler};
+}
+
+module.exports = Upload;
