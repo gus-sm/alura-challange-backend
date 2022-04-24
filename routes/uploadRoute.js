8d2ad14d8cc59cfd6903b28bb608e29a0a440339
gus-sm <gustavo1sad@gmail.com>
2022-04-24 07:49:44 -0300

    Alteracao de rota GET

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,10 +1,11 @@
 const router = require('express').Router(),
       csvStreamReader= require('../helpers/csvStreamReader'),
       Connection  = require('../database/dbConnection')(),
-      Upload = require('../controllers/Upload')(),
-      Transaction = require('../controllers/Transactions')(csvStreamReader, Connection);
 
-router.get('/', Transaction.listTransactions);
+Upload = require('../controllers/Uploads')(),
+Transaction = require('../controllers/Transactions')(csvStreamReader, Connection);
+
+router.get('/upload', Transaction.listTransactions);
 router.post('/upload', Upload.uploadHandler, Transaction.createTransaction);
 
 module.exports = router;
