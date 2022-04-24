ac0d01a555642aff16c1a216ae93601bbc7f7fc2
gus-sm <gustavo1sad@gmail.com>
2022-04-24 06:14:32 -0300

    Definido atributos de transactions como obrigatorios no banco e criação de schema para usuarios

---
+++ models/Users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        id: {type: String},
+        name: {type: String, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true}
+    },
+    { timestamps: true }
+);
+
+const Users = mongoose.model('Transactions', TransactionSchema);
+
+module.exports = { Users };
