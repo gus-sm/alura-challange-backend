a91ce85aa83df544642ecc15e521a161d8382cfb
gus-sm <gustavo1sad@gmail.com>
2022-04-26 02:20:15 -0300

    Alterado script de inicialização do BD para criação de usuário admin da aplicação

--- config/database/init-mongo.js
+++ config/database/init-mongo.js
@@ -7,4 +7,19 @@ db.createUser({
             db: "alura-challange-db"
         }
     ] 
-})
+});
+
+db = new Mongo().getDB("alura-challange-db");
+
+db.createCollection('Users', { capped: false });
+//db.createCollection('test', { capped: false });
+
+db.users.insert([
+    { 
+        name: "Admin",
+        email: "admin@email.com.br",
+        password: "$2a$10$V2W0kc7J0YD8DP/4K1WLSeFnvqrZlQ0yrq4HIs7D3..8uxkxD/q/y",
+        active: true
+    },
+
+]);
